f2c9594de961c7a0f34c0c90d9716207056dc73c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-04-16 14:00:30 +0200

    feat: add homework 6 solution

---
+++ homeworks/hw-6/hw6.py
@@ -0,0 +1,62 @@
+import random           # to generate random points
+import numpy as np      # to compute the distance of each point from the origin
+
+#------------------------------------------------ global constants
+TRIALS = 100                                    # Number of trials per precision
+PRECISIONS = [10**(-i) for i in range(1, 8)]    # List of precisions to try
+MAXIMUM_POINTS = 10000                          # Maximum number of points to use
+
+################################################################################
+# generate_r:: generates random value for r                                    #
+################################################################################
+def generate_r():
+    # generate a random point
+    x = random.uniform(0, 1)    # random value for x between 0 & 1
+    y = random.uniform(0, 1)    # random value for y between 0 & 1
+
+    # calculate the value of r
+    return np.sqrt(x**2 + y**2)
+
+################################################################################
+# print_results:: prints average pi & num of attempts for each precision       #
+################################################################################
+def print_results(attemps, pi_val, precision):
+    if attemps > 0:
+        pi_avg = pi_val / attemps
+        print(f'{precision} success {attemps} times {pi_avg}')
+    else:
+        print(f'{precision} no success')
+
+################################################################################
+# main:: project entry point                                                   #
+################################################################################
+def main():
+    for precision in PRECISIONS:
+        success_attemps = 0     # init number of successfull attempts
+        pi_val = 0              # init the value of pi
+
+        for _ in range(TRIALS):
+            points_num = 0      # init number of total points
+            points_inside = 0   # init number of total points inside circel
+
+            while points_num < MAXIMUM_POINTS:
+                # generate random value for r
+                r = generate_r()
+
+                # check if r is inside the circle or not
+                if (r <= 1):
+                    points_inside += 1      # increment number of points inside
+
+                points_num += 1
+
+                # check if we achieved the precision
+                if abs(4*points_inside/points_num - np.pi) < precision:
+                    success_attemps += 1
+                    pi_val += 4*points_inside/points_num
+                    break
+
+        # print results
+        print_results(success_attemps, pi_val, precision)
+
+# start script
+main()
